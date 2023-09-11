fce91f70d946840685ae5528ee3953a488f8de81
Carlos Eduardo da Silva <carlos.silva470@fatec.sp.gov.br>
2023-09-10 23:53:49 -0300

    adicionar idade

--- categoriaidade.py
+++ categoriaidade.py
@@ -1,17 +1,17 @@
 # Obtém a idade do nadador
-idade = int(input("Digite a idade do nadador: "))
+idad = int(input("Digite a idade do nadador: "))
 
 # Determina a categoria com base na idade
-if 5 <= idade <= 7:
+if 5 <= idad <= 7:
     categoria = "Infantil"
-elif 8 <= idade <= 10:
+elif 8 <= idad <= 10:
     categoria = "Juvenil"
-elif 11 <= idade <= 15:
+elif 11 <= idad <= 15:
     categoria = "Adolescente"
-elif 16 <= idade <= 30:
+elif 16 <= idad <= 30:
     categoria = "Adulto"
 else:
     categoria = "Sênior"
 
 # Exibe a categoria do nadador
-print(f"O nadador de {idade} anos pertence à categoria {categoria}.")
+print(f"O nadador de {idad} anos pertence à categoria {categoria}.")
